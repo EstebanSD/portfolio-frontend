93a4525574a02533c9049c2236a6316b294dfb49
Esteban Salvay Dilena <estebansaldil22@gmail.com>
2025-08-18 13:12:15 -0300

    refactor: improuve input filters

---
+++ src/components/common/SearchFilterInputUrl.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { useUrlParams } from '@/hooks';
+import { SearchInputBase, SearchInputBaseProps } from './SearchInputBase';
+
+interface UrlModeProps extends SearchInputBaseProps {
+  basePath: string;
+  debounceMs?: number;
+  defaultValue?: string;
+}
+
+export function SearchFilterInputUrl({
+  paramKey,
+  label,
+  placeholder,
+  className,
+  basePath,
+  debounceMs = 600,
+  defaultValue,
+}: UrlModeProps) {
+  const { value, setValue } = useUrlParams(paramKey, {
+    basePath,
+    debounceMs,
+    defaultValue,
+  });
+
+  return (
+    <SearchInputBase
+      paramKey={paramKey}
+      label={label}
+      placeholder={placeholder}
+      className={className}
+      value={value}
+      onChange={setValue}
+    />
+  );
+}
