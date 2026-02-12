5e1561c10906b7893a1199a0620ec833addfc6ec
Esteban Salvay Dilena <estebansaldil22@gmail.com>
2026-02-12 11:35:22 -0300

    chore: initial state

--- src/app/(auth)/admin/skills/page.tsx
+++ src/app/(auth)/admin/skills/page.tsx
@@ -1,3 +1,12 @@
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div className="space-y-6 max-w-7xl mx-auto">
+      <div>
+        <h1 className="text-3xl font-bold">Skills Management</h1>
+        <p className="text-muted-foreground">
+          Take charge of your skills and keep them well organized.
+        </p>
+      </div>
+    </div>
+  );
 }
